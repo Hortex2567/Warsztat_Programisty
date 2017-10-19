602e3d4d87c7f11e8a86e761d2ff69bf3a8da231
Hortex2567 <32922646+Hortex2567@users.noreply.github.com>
2017-10-19 09:05:09 +0200

    Add files via upload

--- test1.c
+++ test1.c
@@ -3,12 +3,13 @@
 int main()
 {
 	double pi, pole, r;
-	
 	pi=3.14;
-	printf("Podaj promien kola\n");
-	scanf("%f", &r);
-	printf("%f", &r);
 	
-	pole = r*r*pi;
+	printf("Podaj promien kola:\n");
+	scanf("%lf", &r);
+	
+	pole = (r*r)*pi;
 	printf("Pole kola jest rowne %f", pole);
+	
+	return 0;
 }
